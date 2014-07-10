16671ee3a0afeec91431deb656aeaab906d4abb8
Átila Camurça <camurca.home@gmail.com>
2014-07-10 11:22:12 -0300

    SalvarActivity carregando os elementos EditText do arquivo xml.

--- app/src/main/java/exemplo/android/guia/contatos/view/SalvarActivity.java
+++ app/src/main/java/exemplo/android/guia/contatos/view/SalvarActivity.java
@@ -2,13 +2,24 @@ package exemplo.android.guia.contatos.view;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
 import exemplo.android.guia.contatos.R;
 
 public class SalvarActivity extends ActionBarActivity {
+
+    private EditText etNome, etTelefone, etEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_salvar);
+        carregar();
+    }
+
+    private void carregar() {
+        etNome = (EditText) findViewById(R.id.et_nome);
+        etTelefone = (EditText) findViewById(R.id.et_telefone);
+        etEmail = (EditText) findViewById(R.id.et_email);
     }
 }
